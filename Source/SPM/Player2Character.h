cdc64ff9dfefadc8744b2631e520f07466ee30be
MathildaLindkvist <mathilda@qns.se>
2025-04-14 13:00:37 +0200

    Merge from main

---
+++ Source/SPM/Player2Character.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SPMCharacter.h"
+#include "Player2Character.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPM_API APlayer2Character : public ASPMCharacter
+{
+	GENERATED_BODY()
+	
+};
