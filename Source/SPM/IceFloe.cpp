e941a99f41ffd8e0706af5e9e2b82d8baffcbebe
Nora Vidström <nora.vidstroem@gmail.com>
2025-04-14 15:22:52 +0200

    icefloe

---
+++ Source/SPM/IceFloe.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IceFloe.h"
+
+// Sets default values
+AIceFloe::AIceFloe()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AIceFloe::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AIceFloe::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AIceFloe::BreakIceFloe()
+{
+	
+}
+void AIceFloe::respawnIceFloe()
+{
+	
+}
+
+
