12551134eb13138020d6085988d08371313276bf
Kajsaherge <kajsaherge@gmail.com>
2025-04-15 20:25:31 +0200

    Småändringar

--- Source/SPM/CharacterBase.cpp
+++ Source/SPM/CharacterBase.cpp
@@ -55,7 +55,6 @@ ACharacterBase::ACharacterBase()
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
-	NoiseEmitter = CreateDefaultSubobject<UPawnNoiseEmitterComponent>(TEXT("PawnNoiseEmitter"));
 	BodyTempComponent = CreateDefaultSubobject<UBodyTemperature>(TEXT("BodyTemperature"));
 	HealthComponent = CreateDefaultSubobject<UHealth>(TEXT("Health"));
 
