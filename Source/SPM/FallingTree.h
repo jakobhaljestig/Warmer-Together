92f523357ad75653b600256868d33dea1c1aef7b
RobinOhman <144446722+RobinOhman@users.noreply.github.com>
2025-04-25 09:12:45 +0200

    FallingTree class created

---
+++ Source/SPM/FallingTree.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FallingTree.generated.h"
+
+UCLASS()
+class SPM_API AFallingTree : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFallingTree();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* StaticMesh = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	FRotator FallingAngle;
+
+	void Fall();
+
+};
