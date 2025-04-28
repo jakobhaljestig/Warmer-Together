e049f7941307740497982fac5334e49f0a413fff
RobinOhman <144446722+RobinOhman@users.noreply.github.com>
2025-04-28 13:34:40 +0200

    Update GrabComponent.h

--- Source/SPM/GrabComponent.h
+++ Source/SPM/GrabComponent.h
@@ -20,11 +20,15 @@ public:
 	UGrabComponent();
 	virtual ~UGrabComponent() override;
 
+
+	//Toggle's between Grab and Release
 	UFUNCTION(BlueprintCallable)
 	virtual void GrabAndRelease();
 
+	//Grab object
 	virtual void Grab();
 
+	//Release object
 	virtual void Release();
 
 protected:
@@ -59,10 +63,11 @@ protected:
 	//Used to trigger different effects upon grabbing something
 	virtual void GrabEffect();
 
-	//Used to trigger effects opn releasing something
+	//Used to trigger effects upon releasing something
 	virtual void ReleaseEffect();
 
 private:
+	//Gets PhysicsHandleComponent
 	UPhysicsHandleComponent* GetPhysicsHandle() const;
 
 public:	
