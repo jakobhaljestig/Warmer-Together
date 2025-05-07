071829ca64795a7ea1c96cd7252df0fa0b346980
RobinOhman <144446722+RobinOhman@users.noreply.github.com>
2025-05-07 13:07:11 +0200

    WaterCollider

---
+++ Source/SPM/FreezingTouchComponent.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FreezingTouchComponent.h"
+
+#include "CharacterBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values for this component's properties
+UFreezingTouchComponent::UFreezingTouchComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+
+}
+
+
+// Called when the game starts
+void UFreezingTouchComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	TriggerBox = GetOwner()->GetComponentByClass<UBoxComponent>();
+}
+
+
+// Called every frame
+void UFreezingTouchComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (TriggerBox != nullptr)
+	{
+		TArray<UPrimitiveComponent*> OverlappingActors;
+		TriggerBox->GetOverlappingComponents(OverlappingActors);
+		for (UPrimitiveComponent* Actor : OverlappingActors)
+		{
+			if (ACharacterBase* Character = Cast<ACharacterBase>(Actor->GetOwner()))
+			{
+				if (Character->GetComponentByClass<UBodyTemperature>()->GetTempPercentage() > 0)
+					Character->GetComponentByClass<UBodyTemperature>()->ModifyTemperature(-CoolDownRate*DeltaTime);
+				else
+				{
+					Character->GetComponentByClass<UHealth>()->TakeDamage(CoolDownRate * DeltaTime);
+				}
+				Character->GetComponentByClass<UCharacterMovementComponent>()->SetMovementMode(MOVE_Swimming);
+			}
+		}
+	}
+	
+
+	// ...
+}
+
---
+++ Source/SPM/FreezingTouchComponent.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AdaptiveWeatherSystem.h"
+#include "Components/ActorComponent.h"
+#include "Components/BoxComponent.h"
+#include "FreezingTouchComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SPM_API UFreezingTouchComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UFreezingTouchComponent();
+
+	UPROPERTY(EditAnywhere)
+	float CoolDownRate;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = true))
+	UBoxComponent* TriggerBox;
+
+	
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+};
