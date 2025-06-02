389f138e410053cf5581e41ea2ffb6d836f26e16
Nora Vidström <144795280+NoraVstrom@users.noreply.github.com>
2025-06-02 16:33:18 +0200

    lite fix i climb igen

--- Source/SPM/ClimbComponent.cpp
+++ Source/SPM/ClimbComponent.cpp
@@ -76,7 +76,7 @@ void UClimbComponent::StartClimb(FHitResult Hit)
 		//position
 		FVector WallDirection = -Arrow->GetForwardVector(); 
 		FVector BasePosition = Hit.ImpactPoint;
-		FVector AttachPosition = BasePosition + WallDirection * 5.0f;
+		FVector AttachPosition = BasePosition + WallDirection * 40.0f;
 		ClimbCharacter->SetActorLocation(AttachPosition);
 
 		//Gör så spelaren roteras åt samma håll som pilen
@@ -147,8 +147,6 @@ void UClimbComponent::FinishClimbUp()
 }
 
 
-
-
 bool UClimbComponent::ClimbingInReach(FHitResult& HitResult) const
 {
 	if (!ClimbCharacter) return false;
@@ -170,20 +168,26 @@ bool UClimbComponent::ClimbingInReach(FHitResult& HitResult) const
 
 	DrawDebugLine(GetWorld(), Start, End, FColor::Green, false, 1.5f, 0, 2.0f);
 
-	//UPrimitiveComponent* Component = HitResult.GetComponent();
+	
 
-	if (bHit && HitResult.GetActor()->ActorHasTag("Climbable"))
+	if (bHit)
 	{
-		TArray<UActorComponent*> Components = HitResult.GetActor()->GetComponentsByTag(UBoxComponent::StaticClass(), "ClimbZone");
-		for (UActorComponent* Comp : Components)
+		UPrimitiveComponent* Component = HitResult.GetComponent();
+		
+		if (Component && Component->ComponentHasTag("Climbable"))
 		{
-			UBoxComponent* Box = Cast<UBoxComponent>(Comp);
-			if (Box && Box->IsOverlappingActor(ClimbCharacter))
+			TArray<UActorComponent*> Components = HitResult.GetActor()->GetComponentsByTag(UBoxComponent::StaticClass(), "ClimbZone");
+			for (UActorComponent* Comp : Components)
 			{
-				return true;
+				UBoxComponent* Box = Cast<UBoxComponent>(Comp);
+				if (Box && Box->IsOverlappingActor(ClimbCharacter))
+				{
+					return true;
+				}
 			}
 		}
 	}
+	
 	return false;
 }
 
